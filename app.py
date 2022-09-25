cca142b766e4a5c37a63e71541e7098c3e79f0e1
Dante <d.dante03@gmail.com>
2022-09-25 11:45:56 +0200

    Connessione con DB

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, Response, jsonify
-import sqlite3
-from sqlite3 import Error
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
---
+++ connection.py
@@ -0,0 +1,13 @@
+import sqlite3
+from sqlite3 import Error
+
+database = r"./data/db.sqlite"
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error:
+        print(Error)
+    conn.row_factory = sqlite3.Row 
+    return conn
